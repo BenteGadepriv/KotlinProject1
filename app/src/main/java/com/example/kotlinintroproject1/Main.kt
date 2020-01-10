b06072fdf496e67da5945a1a8d825607a5d4b190
BenteGadepriv <bente_gade@web.de>
2020-01-10 14:29:57 +0100

    Add Companion Object

--- app/src/main/java/com/example/kotlinintroproject1/Main.kt
+++ app/src/main/java/com/example/kotlinintroproject1/Main.kt
@@ -3,6 +3,5 @@ package com.example.kotlinintroproject1
 
 fun main(args: Array<String>) {
     val user = User("Max", "Mustermann", 55)
-    println(user.age)
-    user.age = 56
+    println(User.USER_TAG)
 }
--- app/src/main/java/com/example/kotlinintroproject1/User.kt
+++ app/src/main/java/com/example/kotlinintroproject1/User.kt
@@ -1,6 +1,7 @@
 package com.example.kotlinintroproject1
 
-class User(val firstName: String, val lastName: String, var _age: Int){
-    val age
-        get() = _age
+class User(val firstName: String, val lastName: String, val age: Int){
+    companion object {
+        const val USER_TAG = "user"
+    }
 }
